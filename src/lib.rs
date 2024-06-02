e6749015ee954ac25dcc1e5a6b0ec3e71df2cd54
Tyr Chen <tyr.chen@gmail.com>
2024-06-02 10:55:20 -0700

    feature: init the napi example project

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![deny(clippy::all)]
+
+use napi_derive::napi;
+
+#[napi]
+pub fn plus(a: u32, b: u32) -> u32 {
+  a + b
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_plus() {
+    assert_eq!(plus(1, 2), 3);
+  }
+}
