cc34959bc227d1415eb476f2359fc7d62ed46304
Tyr Chen <tyr.chen@gmail.com>
2024-06-02 11:32:33 -0700

    chore: refactor code for better rust related tooling

--- algo/src/algo.rs
+++ algo/src/algo.rs
@@ -3,55 +3,55 @@ use std::hash::{Hash, Hasher};
 
 #[derive(Debug, Clone)]
 pub struct Algo {
-  pub r#type: AlgoType,
+    pub r#type: AlgoType,
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum AlgoType {
-  Blake3,
-  Default,
+    Blake3,
+    Default,
 }
 
 impl Algo {
-  pub fn new(r#type: AlgoType) -> Self {
-    Self { r#type }
-  }
-
-  pub fn hash(&self, v: String) -> String {
-    match self.r#type {
-      AlgoType::Blake3 => {
-        let hash = blake3::hash(v.as_bytes());
-        hash.to_string()
-      }
-      AlgoType::Default => {
-        let mut hasher = DefaultHasher::new();
-        v.hash(&mut hasher);
-        hasher.finish().to_string()
-      }
+    pub fn new(r#type: AlgoType) -> Self {
+        Self { r#type }
     }
-  }
 
-  pub fn get_name(&self) -> &str {
-    match self.r#type {
-      AlgoType::Blake3 => "blake3",
-      AlgoType::Default => "default",
+    pub fn hash(&self, v: String) -> String {
+        match self.r#type {
+            AlgoType::Blake3 => {
+                let hash = blake3::hash(v.as_bytes());
+                hash.to_string()
+            }
+            AlgoType::Default => {
+                let mut hasher = DefaultHasher::new();
+                v.hash(&mut hasher);
+                hasher.finish().to_string()
+            }
+        }
     }
-  }
+
+    pub fn get_name(&self) -> &str {
+        match self.r#type {
+            AlgoType::Blake3 => "blake3",
+            AlgoType::Default => "default",
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn hash_should_work() {
-    let algo = Algo::new(AlgoType::Blake3);
-    assert_eq!(
-      algo.hash("hello".to_string()),
-      "b0a7f6e1bcb9e8f708e4c3c1b9d1e5b0b0a7f6e1bcb9e8f708e4c3c1b9d1e5b0"
-    );
+    #[test]
+    fn hash_should_work() {
+        let algo = Algo::new(AlgoType::Blake3);
+        assert_eq!(
+            algo.hash("hello".to_string()),
+            "ea8f163db38682925e4491c5e58d4bb3506ef8c14eb78a86e908c5624a67200f"
+        );
 
-    let algo = Algo::new(AlgoType::Default);
-    assert_eq!(algo.hash("hello".to_string()), "17468397271165231797");
-  }
+        let algo = Algo::new(AlgoType::Default);
+        assert_eq!(algo.hash("hello".to_string()), "16156531084128653017");
+    }
 }
--- node-binding/index.js
+++ node-binding/index.js
@@ -37,7 +37,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./package-template.android-arm64.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-android-arm64')
+            nativeBinding = require('@napi-rs/node-binding-android-arm64')
           }
         } catch (e) {
           loadError = e
@@ -49,7 +49,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./package-template.android-arm-eabi.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-android-arm-eabi')
+            nativeBinding = require('@napi-rs/node-binding-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -67,7 +67,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./package-template.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-win32-x64-msvc')
+            nativeBinding = require('@napi-rs/node-binding-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -79,7 +79,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./package-template.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-win32-ia32-msvc')
+            nativeBinding = require('@napi-rs/node-binding-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -91,7 +91,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./package-template.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-win32-arm64-msvc')
+            nativeBinding = require('@napi-rs/node-binding-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -107,7 +107,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./package-template.darwin-universal.node')
       } else {
-        nativeBinding = require('@napi-rs/package-template-darwin-universal')
+        nativeBinding = require('@napi-rs/node-binding-darwin-universal')
       }
       break
     } catch {}
@@ -118,7 +118,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./package-template.darwin-x64.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-darwin-x64')
+            nativeBinding = require('@napi-rs/node-binding-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -130,7 +130,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./package-template.darwin-arm64.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-darwin-arm64')
+            nativeBinding = require('@napi-rs/node-binding-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -149,7 +149,7 @@ switch (platform) {
       if (localFileExisted) {
         nativeBinding = require('./package-template.freebsd-x64.node')
       } else {
-        nativeBinding = require('@napi-rs/package-template-freebsd-x64')
+        nativeBinding = require('@napi-rs/node-binding-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -164,7 +164,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./package-template.linux-x64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/package-template-linux-x64-musl')
+              nativeBinding = require('@napi-rs/node-binding-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
@@ -175,7 +175,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./package-template.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/package-template-linux-x64-gnu')
+              nativeBinding = require('@napi-rs/node-binding-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -189,7 +189,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./package-template.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/package-template-linux-arm64-musl')
+              nativeBinding = require('@napi-rs/node-binding-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
@@ -200,7 +200,7 @@ switch (platform) {
             if (localFileExisted) {
               nativeBinding = require('./package-template.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/package-template-linux-arm64-gnu')
+              nativeBinding = require('@napi-rs/node-binding-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -213,7 +213,7 @@ switch (platform) {
           if (localFileExisted) {
             nativeBinding = require('./package-template.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-linux-arm-gnueabihf')
+            nativeBinding = require('@napi-rs/node-binding-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
